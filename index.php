01842919e6b0d786e96906433c904ccb7a41d3e6
Kevin aubry <kevin-aubry@orange.fr>
2018-09-20 16:19:02 +0200

    tool

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 
+
+
+
+
+$foo = "4h20";
+
+
 function dd($option='', $arg=''){
     echo "<style>
             .dd_container{
                 border : 3px dashed black;
                 text-align: center;
-                width: 220px;
+                width: 300px;
                 padding: 3px;
+//                position: fixed;
+//                top :0;
+//                left: 0;
             } 
             
             .dd_container ul{
@@ -15,19 +25,53 @@ function dd($option='', $arg=''){
             
         </style>";
 
-    $result ='';
 
     switch ($option) {
         //Doc
     case 0:
-		$result .= '<div class="dd_container">== doc ==<br>';
-		$result .= '$options possibles values :';
-		$result .= '<ul><li>1 : var_dump()</li>';
-		$result .= '<li>2 : print_r()</li>';
-		$result .= '<li>3 : console.log()</li>';
-		$result .= '<li>4 : get_defined_var()</li>';
-		$result .= '<li>5 : get_defined_constants()</li></ul>';
-        $result .= '$arg possibles values : whatever</div>';
+		echo '<div class="dd_container"><h3>== tools ==</h3>';
+        echo '<form method="get">';
+        echo '<ul><li>1 : var_dump(<input name="vd" type="text" placeholder="$...">)</li>';
+        echo '<li>2 : print_r(<input name="pr" type="text" placeholder="$...">)</li>';
+        echo '<li>3 : console.log(<input name="cl" type="text" placeholder="$...">)</li>';
+        echo '<li>4 : get_defined_var(<input name="gdv" type="checkbox">)</li>';
+        echo '<li>5 : get_defined_constants(<input name="gdc" type="checkbox">)</li></ul>';
+        echo '<input type="submit">';
+		echo '</form></div>';
+
+
+
+
+        if(isset($_GET['vd']) && !empty($_GET['vd'])){
+            global $$_GET['vd'];
+            var_dump($$_GET['vd']);
+        }
+        if(isset($_GET['pr']) && !empty($_GET['pr'])){
+            global $$_GET['pr'];
+            echo '<pre>';
+            print_r($$_GET['pr']);
+            echo '</pre>';
+        }
+
+        if(isset($_GET['cl']) && !empty($_GET['cl'])){
+            global $$_GET['cl'];
+            echo '<script>';
+            echo 'console.log('. json_encode( $$_GET['cl'] ) .')';
+            echo '</script>';
+        }
+
+        if(isset($_GET['gdv']) && !empty($_GET['gdv'])){
+            echo '<pre>';
+            print_r($GLOBALS += get_defined_vars());
+            echo '</pre>';
+        }
+
+        if(isset($_GET['gdc']) && !empty($_GET['gdc'])){
+            echo '<pre>';
+            print_r(get_defined_constants());
+            echo '</pre>';
+        }
+
     break;
 
     case 1:
@@ -61,5 +105,22 @@ function dd($option='', $arg=''){
 
     }
 
-    echo $result;
 }
+
+?>
+
+<!--<form action="" method="get">-->
+<!---->
+<!--    <textarea name="json" id="" cols="30" rows="10"></textarea>-->
+<!--    <input type="submit">-->
+<!--</form>-->
+
+<?php
+//if(isset($_GET['json'])){
+//    dd(2, json_decode($_GET['json']));
+//}
+
+
+
+
+    dd(0);
