4cfb041cec48985e7981e5f3abdb40db7efeb498
Kevin aubry <kevin-aubry@orange.fr>
2018-09-20 16:34:23 +0200

    tool

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <?php
 
 
-
-
-
-$foo = "4h20";
 
 
 function dd($option='', $arg=''){
@@ -14,13 +11,32 @@ function dd($option='', $arg=''){
                 text-align: center;
                 width: 300px;
                 padding: 3px;
-//                position: fixed;
-//                top :0;
-//                left: 0;
+                position: fixed;
+                bottom :0;
+                left: 0;
+                display: none;
             } 
             
             .dd_container ul{
                text-align: left; 
+            }
+            
+            .dd_arrow{
+            width: 50px;
+            height:50px;
+            background-color: #00ccff;
+            position: fixed;
+            bottom :0;
+            left: 0;
+            }
+            
+            .dd_close{
+                width: 15px;
+                height:15px;
+                background-color: #00ccff;
+                position: absolute;
+                top :0;
+                right: 0;
             }
             
         </style>";
@@ -29,7 +45,9 @@ function dd($option='', $arg=''){
     switch ($option) {
         //Doc
     case 0:
-		echo '<div class="dd_container"><h3>== tools ==</h3>';
+		echo '<div class="dd_container">';
+		echo '<div class="dd_close" onclick="dd_close()"></div>';
+		echo '<h3>== tools ==</h3>';
         echo '<form method="get">';
         echo '<ul><li>1 : var_dump(<input name="vd" type="text" placeholder="$...">)</li>';
         echo '<li>2 : print_r(<input name="pr" type="text" placeholder="$...">)</li>';
@@ -38,6 +56,34 @@ function dd($option='', $arg=''){
         echo '<li>5 : get_defined_constants(<input name="gdc" type="checkbox">)</li></ul>';
         echo '<input type="submit">';
 		echo '</form></div>';
+
+		echo '<div class="dd_arrow" onclick="dd_show()"></div>';
+
+
+
+		?>
+
+        <script>
+            function dd_show(){
+                $('.dd_arrow').fadeOut('slow','linear');
+                $('.dd_container').fadeIn('slow','linear');
+            }
+
+            function dd_close(){
+                $('.dd_arrow').fadeIn('slow','linear');
+                $('.dd_container').fadeOut('slow','linear');
+            }
+
+        </script>
+
+<?php
+
+
+
+
+
+
+
 
 
 
@@ -106,21 +152,6 @@ function dd($option='', $arg=''){
     }
 
 }
-
-?>
-
-<!--<form action="" method="get">-->
-<!---->
-<!--    <textarea name="json" id="" cols="30" rows="10"></textarea>-->
-<!--    <input type="submit">-->
-<!--</form>-->
-
-<?php
-//if(isset($_GET['json'])){
-//    dd(2, json_decode($_GET['json']));
-//}
-
-
 
 
     dd(0);
