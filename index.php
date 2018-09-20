b128f334d5f6afa52713a0d6d607de09d96cd0ae
Kevin aubry <kevin-aubry@orange.fr>
2018-09-20 17:05:45 +0200

    tool

--- index.php
+++ index.php
@@ -48,8 +48,8 @@ function dd($option='', $arg=''){
 		echo '<div class="dd_container">';
 		echo '<div class="dd_close" onclick="dd_close()"></div>';
 		echo '<h3>== tools ==</h3>';
-        echo '<form method="get">';
-        echo '<ul><li>1 : var_dump(<input name="vd" type="text" placeholder="$...">)</li>';
+        echo '<form method="POST" id="dd_form">';
+        echo '<ul><li>1 : var_dump(<input name="vd" class="dd_vd" type="text" placeholder="$...">)</li>';
         echo '<li>2 : print_r(<input name="pr" type="text" placeholder="$...">)</li>';
         echo '<li>3 : console.log(<input name="cl" type="text" placeholder="$...">)</li>';
         echo '<li>4 : get_defined_var(<input name="gdv" type="checkbox">)</li>';
@@ -74,6 +74,27 @@ function dd($option='', $arg=''){
                 $('.dd_container').fadeOut('slow','linear');
             }
 
+
+
+            $("#dd_form").click('submit', function (e) {
+
+                var vd = $(".dd_vd").val();
+                    jQuery.ajax({
+                        type: "POST",
+                        url: "traitement.php",
+                        data: {vd : vd},
+
+
+                        success: function(result){
+                            console.log(result);
+                        }
+                    });
+                console.log(vd);
+                    e.preventDefault();
+                    return false;
+                });
+
+
         </script>
 
 <?php
@@ -82,43 +103,7 @@ function dd($option='', $arg=''){
 
 
 
-
-
-
-
-
-
-        if(isset($_GET['vd']) && !empty($_GET['vd'])){
-            global $$_GET['vd'];
-            var_dump($$_GET['vd']);
-        }
-        if(isset($_GET['pr']) && !empty($_GET['pr'])){
-            global $$_GET['pr'];
-            echo '<pre>';
-            print_r($$_GET['pr']);
-            echo '</pre>';
-        }
-
-        if(isset($_GET['cl']) && !empty($_GET['cl'])){
-            global $$_GET['cl'];
-            echo '<script>';
-            echo 'console.log('. json_encode( $$_GET['cl'] ) .')';
-            echo '</script>';
-        }
-
-        if(isset($_GET['gdv']) && !empty($_GET['gdv'])){
-            echo '<pre>';
-            print_r($GLOBALS += get_defined_vars());
-            echo '</pre>';
-        }
-
-        if(isset($_GET['gdc']) && !empty($_GET['gdc'])){
-            echo '<pre>';
-            print_r(get_defined_constants());
-            echo '</pre>';
-        }
-
-    break;
+        break;
 
     case 1:
 		var_dump($arg);
